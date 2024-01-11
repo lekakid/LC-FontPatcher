7c1b557730bad8c3c6c24cdbeebe88d8f966ba24
LeKAKiD <lekakid@kakao.com>
2024-01-11 23:08:39 +0900

    Add 3270 font cases

--- FontPatcher/Loader.cs
+++ FontPatcher/Loader.cs
@@ -30,16 +30,32 @@ class FontLoader
     [HarmonyPostfix, HarmonyPatch(typeof(TextMeshProUGUI), "Awake")]
     static void PostFixAwake(TextMeshProUGUI __instance)
     {
-        Plugin.Instance.LogInfo(__instance.font.name);
-        string prevFontName = __instance.font.name;
+        string prevFontName = __instance.font?.name;
 
         switch (prevFontName)
         {
             case "3270-Regular SDF":
-                __instance.font = NormalFont;
+            case "3270-HUDIngame":
+            case "3270-HUDIngameB":
+            case "3270-HUDIngame - Variant":
+            case "b":
+                SwapFont(__instance, NormalFont);
                 break;
             default:
                 break;
         }
+    }
+
+    static void SwapFont(TextMeshProUGUI instance, TMP_FontAsset nextFont)
+    {
+        string prevFontName = instance.font?.name;
+        string target = $"{instance.transform.name}({instance.text.Substring(0, Math.Min(instance.text.Length, 10))})";
+
+        TMP_FontAsset prevFont = instance.font;
+        instance.font = nextFont;
+        instance.fontMaterial = nextFont.material;
+        instance.font.fallbackFontAssetTable.Add(prevFont);
+
+        Plugin.Instance.LogInfo($"{target} | {prevFontName} => {nextFont.name}");
     }
 }
